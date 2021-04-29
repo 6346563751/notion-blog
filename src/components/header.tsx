758b5a9371946386b1a6a0b9dc266514ffbbacb9
bob <bob@pop-os.localdomain>
2021-04-29 14:48:27 +0200

    desc

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Contact', page: '/contact' },
 ]
 
-const ogImageUrl = 'https://media.giphy.com/media/fwzqaadNAbyUw444ey/giphy.gif'
+const ogImageUrl = 'https://media.giphy.com/media/QBA56XrGs3i2zMiryz/giphy.gif'
 
 const Header = ({ titlePre = '' }) => {
   const { pathname } = useRouter()
@@ -18,7 +18,15 @@ const Header = ({ titlePre = '' }) => {
     <header className={styles.header}>
       <Head>
         <title>{titlePre ? `${titlePre} |` : ''} Bob</title>
-        <meta name="description" content="12" />
+        <meta
+          name="description"
+          content="みなさん、こんにちは！私のブログへようこそ、あなたは本当にこのページへの道を失ったに違いありません。"
+        />
+        <meta
+          property="og:description"
+          content="みなさん、こんにちは！私のブログへようこそ、あなたは本当にこのページへの道を失ったに違いありません。"
+        />
+
         <meta name="og:title" content="Bob" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
